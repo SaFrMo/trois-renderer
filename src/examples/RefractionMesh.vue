37353f8c503e9247406c4a443614f76d037815a8
Sander Moolin <sandermoolin@gmail.com>
2021-07-08 15:39:04 -0400

    Revert "Revert "snapshot""
    This reverts commit 2000d3500f28b1e0b7b3cd86cedbba2f3e09c170.

--- src/examples/RefractionMesh.vue
+++ src/examples/RefractionMesh.vue
@@ -7,19 +7,48 @@
             :roughness="0"
             :flatShading="true"
         />
+
+        <cubeCamera
+            attach="cubeCamera"
+            v-if="$refs.rt"
+            :args="[0.1, 2000, $refs.rt.$el.target]"
+        />
     </mesh>
+
+    <!-- cube info -->
+    <webGLCubeRenderTarget
+        :args="[
+            256,
+            {
+                mapping: CubeRefractionMapping,
+                format: RGBFormat,
+                generateMipmaps: true,
+                minFilter: LinearMipmapLinearFilter,
+            },
+        ]"
+        ref="rt"
+    />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import {
+    CubeRefractionMapping,
+    RGBFormat,
+    LinearMipmapLinearFilter,
+} from 'three'
 
 export default defineComponent({
     data() {
         return {
             meshRotation: [0, 0, 0],
+            CubeRefractionMapping,
+            RGBFormat,
+            LinearMipmapLinearFilter,
         }
     },
     mounted() {
+        console.log('zzzzzz', this.$refs)
         this.update()
     },
     methods: {
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
 import { createApp, extend } from './renderer'
-import App from './examples/Demo4.vue'
+import App from './examples/Demo1.vue'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-
+import {
+    CubeCamera,
+    // CubeRefractionMapping,
+    // LinearMipmapLinearFilter,
+    // RGBFormat,
+    WebGLCubeRenderTarget,
+} from 'three'
 
 const app = createApp(App)
 
-extend({ app, OrbitControls })
+extend({ app, OrbitControls, CubeCamera, WebGLCubeRenderTarget })
 
 
 app.mount('#app')
