10cc735dc74fe940fcf14ffdc9d8afe1eabe168f
Sander Moolin <sandermoolin@gmail.com>
2021-07-19 09:34:54 -0400

    wip on plane

--- src/examples/Demo1/NoisyPlane.vue
+++ src/examples/Demo1/NoisyPlane.vue
@@ -1,3 +1,19 @@
 <template>
-    <plane> </plane>
+    <mesh :scale-x="50" :scale-y="50" :rotation-x="Math.PI * -0.5" :y="-10">
+        <planeBufferGeometry />
+
+        <!-- create plane material consisting of:
+            - displacement (dispRT.texture) 
+            - normal (normRT.texture)
+        -->
+        <meshBasicMaterial color="red">
+            <!-- create FullScreenQuad pass -->
+
+            <!-- create displacement RT -->
+            <!-- create displacement map ShaderMaterial -->
+
+            <!-- create normal RT -->
+            <!-- create normal map ShaderMaterial -->
+        </meshBasicMaterial>
+    </mesh>
 </template>
