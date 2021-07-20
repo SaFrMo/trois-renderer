e305bf07a197758b7dfa76702992ac65ca9da9d1
Sander Moolin <sandermoolin@gmail.com>
2021-07-20 11:47:49 -0400

    separate insert function

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,16 +1,15 @@
 import { Object3D } from 'three'
 import { createRenderer, Component } from 'vue'
 import { RendererOptions } from '@vue/runtime-core'
-import { createObject, updateAllObjectProps, updateObjectProp } from './objects'
-import { isObject3D, pascalCase } from './lib'
+import { updateObjectProp } from './objects'
+// import { isObject3D, pascalCase } from './lib'
 import { components } from './components'
-import { initTrois, useTrois } from './useThree'
+import { useTrois } from './useThree'
 import { Trois } from './types'
 const trois = useTrois()
-const created: { [key: number]: Trois.Element } = {}
-import { createElement as createElementNodeOp } from './nodeOps'
-import { createElement } from './trois'
-
+export const created: { [key: number]: Trois.Element } = {}
+import { createElement as createElementNodeOp, insert } from './nodeOps'
+import { createElement as createTroisElement } from './trois'
 
 /*
     Elements are `create`d from the outside in, then `insert`ed from the inside out.
@@ -19,101 +18,7 @@ import { createElement } from './trois'
 const nodeOps: RendererOptions<Trois.Node, Trois.Element> = {
     createElement: createElementNodeOp,
 
-    insert: (child, parent, ref?: Trois.Element | null) => {
-        // debug
-        console.log('insert', { child, parent, ref })
-
-        // calculate the ref index because the child's removal may have affected it
-        // parent.children = parent.children || []
-        const refIndex = (ref ? parent?.children?.indexOf(ref) : -1) ?? -1
-        if (refIndex === -1) {
-            // child not present in scene yet (v-if, for example)
-            // parent.children?.push(child)
-            // child.parentNode = parent
-        } else {
-            parent.children?.splice(refIndex, 0, child)
-            child.parentNode = parent
-        }
-
-        // convert type to PascalCase
-        let name = ''
-        if (child.type) {
-            name = pascalCase(child.type)
-        }
-
-        // cancel if no valid name
-        if (!name) return
-
-        // mount dom elements
-        if (child.domElement) {
-            // apply styling
-            Object.keys(child?.props?.style).forEach(key => {
-                (child.domElement?.style ?? {} as any)[key] = (child?.props?.style ?? {})[key]
-            })
-
-            // attach container to parent
-            if (typeof parent === 'string') {
-                const parentEl = document.querySelector(parent) as any as HTMLElement
-                parentEl.appendChild(child.domElement)
-            } else if (parent?.domElement) {
-                parent.domElement.appendChild(child.domElement)
-            }
-
-            return child.domElement
-        }
-
-        const { renderer, scene } = trois
-        if (!renderer.value || !scene.value) return
-
-        // build object instance
-        child.instance = createObject({ name, element: child })
-        updateAllObjectProps({ element: child, props: child.props || {} })
-
-        // notify parent if needed
-        if (child.props?.attach) {
-            parent.attached = {
-                [child.props.attach]: child.instance,
-                ...(parent?.attached || {})
-            }
-        }
-
-        // save vue ID
-        child.vueId = (child as any)?.__vueParentComponent?.uid
-        if (child.vueId === null || child.vueId === undefined) {
-            return
-        }
-        created[child.vueId] = child
-
-        if (child && child.instance && isObject3D(child.instance)) {
-            let parentNode = (child as any).__vueParentComponent?.vnode?.el
-                ?? parent
-            if (!Object.keys(parentNode).length) {
-                parentNode = created[(child as any).__vueParentComponent?.parent?.uid ?? -1]
-            }
-            const parentInstance = parentNode?.instance
-            child.parentNode = parentNode
-
-            if (parent.type === 'canvas') {
-                // we're a scene-level component, so let's go ahead and add ourselves to the scene
-                scene.value.add(child.instance)
-
-                // we'll also need to add any children who have added themselves to our creation queue
-                child.children?.filter(Boolean).forEach(c => {
-                    (child.instance as any as Object3D).add(c.instance as any as Object3D)
-                })
-
-                // reset children array
-                // child.children = []
-            } else if (parentInstance) {
-                // if we're a child of an existing TroisInstance, add ourselves to that instance
-                parentInstance.add(child.instance)
-            } else if (parentNode) {
-                // if we're a child of a nonexistant TroisInstance, tell the node we'll need to
-                // be created when that instance is created
-                parentNode.children.push(child)
-            }
-        }
-    },
+    insert,
 
     remove: (el) => {
         const instance = el?.instance
@@ -129,12 +34,12 @@ const nodeOps: RendererOptions<Trois.Node, Trois.Element> = {
 
     createText: (text) => {
         // console.log('createText', { text })
-        return createElement('', {})
+        return createTroisElement('', {})
     },
 
     createComment: (text) => {
         console.log('createComment', { text })
-        return createElement('', {})
+        return createTroisElement('', {})
     },
 
     setText: (node, text) => {
@@ -147,7 +52,7 @@ const nodeOps: RendererOptions<Trois.Node, Trois.Element> = {
 
     parentNode: (node) => {
         // console.log('parentNode', { node })
-        return createElement('', {})
+        return createTroisElement('', {})
     },
 
     nextSibling: (node) => {
---
+++ src/renderer/nodeOps/insert.ts
@@ -0,0 +1,102 @@
+import { Trois } from '../types'
+import { isObject3D, pascalCase } from '../lib'
+import { createObject, updateAllObjectProps, } from '../objects'
+import { Object3D } from 'three'
+import { useTrois } from '../useThree'
+const trois = useTrois()
+import { created } from '..'
+
+
+export const insert = (
+    element: Trois.Element,
+    parent: Trois.Element,
+    ref?: Trois.Element | null
+) => {
+    // debug
+    console.log('insert', { child: element, parent, ref })
+
+    // calculate the ref index because the child's removal may have affected it
+    // parent.children = parent.children || []
+    const refIndex = (ref ? parent?.children?.indexOf(ref) : -1) ?? -1
+    if (refIndex === -1) {
+        // child not present in scene yet (v-if, for example)
+        // parent.children?.push(child)
+        // child.parentNode = parent
+    } else {
+        parent.children?.splice(refIndex, 0, element)
+        element.parentNode = parent
+    }
+
+    // cancel if no valid name
+    if (!element.name) return
+
+    // mount dom elements
+    if (element.domElement) {
+        // apply styling
+        Object.keys(element?.props?.style).forEach(key => {
+            (element.domElement?.style ?? {} as any)[key] = (element?.props?.style ?? {})[key]
+        })
+
+        // attach container to parent
+        if (typeof parent === 'string') {
+            const parentEl = document.querySelector(parent) as any as HTMLElement
+            parentEl.appendChild(element.domElement)
+        } else if (parent?.domElement) {
+            parent.domElement.appendChild(element.domElement)
+        }
+
+        return element.domElement
+    }
+
+    const { renderer, scene } = trois
+    if (!renderer.value || !scene.value) return
+
+    // build object instance
+    element.instance = createObject({ name: element.name, element })
+    updateAllObjectProps({ element, props: element.props || {} })
+
+    // notify parent if needed
+    if (element.props?.attach) {
+        parent.attached = {
+            [element.props.attach]: element.instance,
+            ...(parent?.attached || {})
+        }
+    }
+
+    // save vue ID
+    element.vueId = (element as any)?.__vueParentComponent?.uid
+    if (element.vueId === null || element.vueId === undefined) {
+        return
+    }
+    created[element.vueId] = element
+
+    if (element && element.instance && isObject3D(element.instance)) {
+        let parentNode = (element as any).__vueParentComponent?.vnode?.el
+            ?? parent
+        if (!Object.keys(parentNode).length) {
+            parentNode = created[(element as any).__vueParentComponent?.parent?.uid ?? -1]
+        }
+        const parentInstance = parentNode?.instance
+        element.parentNode = parentNode
+
+        if (parent.type === 'canvas') {
+            // we're a scene-level component, so let's go ahead and add ourselves to the scene
+            scene.value.add(element.instance)
+
+            // we'll also need to add any children who have added themselves to our creation queue
+            element.children?.filter(Boolean).forEach(c => {
+                (element.instance as any as Object3D).add(c.instance as any as Object3D)
+            })
+
+            // reset children array
+            // child.children = []
+        } else if (parentInstance) {
+            // if we're a child of an existing TroisInstance, add ourselves to that instance
+            parentInstance.add(element.instance)
+        } else if (parentNode) {
+            // if we're a child of a nonexistant TroisInstance, tell the node we'll need to
+            // be created when that instance is created
+            parentNode.children.push(element)
+        }
+    }
+}
