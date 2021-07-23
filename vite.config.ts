4290f3df374a60df18664439c3245fcece5e8e6c
Sander Moolin <sandermoolin@gmail.com>
2021-07-23 16:22:41 -0400

    demo pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        demo1: './demo1/index.html',
+        demo4: './demo4/index.html',
+        demo6: './demo6/index.html',
+      }
+    }
+  }
 })
