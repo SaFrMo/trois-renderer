2c9584b57acf5f8a4ca2321600ce8ae28fa32a31
Sander Moolin <sandermoolin@gmail.com>
2021-07-25 16:59:58 -0400

    wip

--- src/examples/InstanceOcean.vue
+++ src/examples/InstanceOcean.vue
@@ -2,12 +2,12 @@
     <TroisCanvas background="white" :cameraPosition="[0, 1.5, 3]">
         <!-- <OrbitControlsWrapper :autoRotate="false" /> -->
 
-        <spotLight color="white" :intensity="1" :position="[0, 150, 0]" />
-        <spotLight color="green" :intensity="0.5" :position="[0, -150, 0]" />
+        <!-- <spotLight color="white" :intensity="1" :position="[0, 150, 0]" /> -->
+        <!-- <spotLight color="green" :intensity="0.5" :position="[0, -150, 0]" /> -->
 
         <instancedMesh :args="[null, null, count]" :scale="0.3" ref="mesh">
             <boxGeometry />
-            <meshPhongMaterial />
+            <meshBasicMaterial color="black" />
         </instancedMesh>
     </TroisCanvas>
 </template>
@@ -16,9 +16,11 @@
 import { defineComponent } from 'vue'
 import { Object3D, InstancedMesh, Vector3 } from 'three'
 import OrbitControlsWrapper from './OrbitControlsWrapper.vue'
+import SimplexNoise from 'simplex-noise'
 
 const scratch = new Object3D()
 const objects = [] as Array<any>
+const simplex = new SimplexNoise()
 
 export default defineComponent({
     components: { OrbitControlsWrapper },
@@ -39,6 +41,7 @@ export default defineComponent({
             const z = -y * 2
 
             const obj = {
+                coords: [x, y],
                 position: [x, y, z],
                 rotation: new Vector3(),
                 rotationSpeed: {
@@ -65,14 +68,26 @@ export default defineComponent({
             const target = (this.$refs.mesh as any).$el
                 .instance as InstancedMesh
 
+            const now = Date.now() * -0.005
+
             for (let i = 0; i < objects.length; i++) {
                 const obj = objects[i]
+                const { coords } = obj
                 scratch.position.set.apply(scratch.position, obj.position)
                 scratch.rotation.x = obj.rotation.x =
                     obj.rotation.x + obj.rotationSpeed.x
                 scratch.rotation.y = obj.rotation.y =
                     obj.rotation.y + obj.rotationSpeed.y
-                scratch.scale.setScalar(obj.scale)
+                scratch.scale.setScalar(
+                    (simplex.noise2D(
+                        (coords[0] + now) * 0.1,
+                        (coords[1] + now) * 0.2
+                    ) *
+                        0.5 +
+                        0.5) *
+                        0.5 +
+                        0.01
+                )
                 scratch.updateMatrix()
                 target.setMatrixAt(i, scratch.matrix)
             }
