9142ab06ff62487ca6f0488a2258700b907d216a
Sander Moolin <sandermoolin@gmail.com>
2021-07-25 15:23:32 -0400

    starting table scene

---
+++ src/examples/Table.vue
@@ -0,0 +1,32 @@
+<template>
+    <TroisCanvas
+        renderer="$attached.renderer"
+        environment="/table/workshop.hdr"
+        :cameraPosition="[0, 0, 10]"
+        background="lightblue"
+    >
+        <OrbitControlsWrapper :autoRotate="false" />
+
+        <!-- renderer fine-tuning -->
+        <WebGLRenderer
+            attach="renderer"
+            :args="[{ antialias: true }]"
+            :outputEncoding="sRGBEncoding"
+        />
+
+        <gltf src="/table/table.glb" :scale="5" :position-y="-2" />
+    </TroisCanvas>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import OrbitControlsWrapper from './OrbitControlsWrapper.vue'
+import { sRGBEncoding } from 'three'
+
+export default defineComponent({
+    components: { OrbitControlsWrapper },
+    data() {
+        return { sRGBEncoding }
+    },
+})
+</script>
