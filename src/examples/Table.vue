6b83ce5666d2a9f9c3132a828791403d6dfd3c9f
Sander Moolin <sandermoolin@gmail.com>
2021-07-25 16:19:52 -0400

    table example

--- src/examples/Table.vue
+++ src/examples/Table.vue
@@ -2,19 +2,64 @@
     <TroisCanvas
         renderer="$attached.renderer"
         environment="/table/workshop.hdr"
-        :cameraPosition="[0, 0, 10]"
-        background="lightblue"
+        :cameraPosition="[0, 2, 4]"
+        background="white"
     >
-        <OrbitControlsWrapper :autoRotate="false" />
+        <OrbitControlsWrapper />
+
+        <pointLight :position="[-5, 3, 0]" :intensity="0.8" />
+        <pointLight :position="[-5, 3, 5]" :intensity="0.8" />
 
         <!-- renderer fine-tuning -->
-        <WebGLRenderer
-            attach="renderer"
-            :args="[{ antialias: true }]"
-            :outputEncoding="sRGBEncoding"
-        />
+        <WebGLRenderer attach="renderer" :args="[{ antialias: true }]" />
 
-        <gltf src="/table/table.glb" :scale="5" :position-y="-2" />
+        <group :rotation-y="Math.PI * -0.15" :y="-0.5">
+            <!-- table -->
+            <!-- <gltf src="/table/table.glb" :scale="5" :position-y="-2.7" /> -->
+
+            <!-- scene -->
+            <gltf src="/table/building_cabin.glb" :rotation-y="Math.PI * 0.5" />
+            <gltf
+                src="/table/building_market.glb"
+                :rotation-y="Math.PI * 0.5"
+                :position="getPosition(2, 0)"
+            />
+            <gltf
+                src="/table/building_house.glb"
+                :rotation-y="Math.PI * 0.5"
+                :position="getPosition(1, 1)"
+            />
+            <gltf
+                src="/table/grass_forest.glb"
+                :rotation-y="Math.PI * 0.5"
+                :position="getPosition(1, -1)"
+            />
+            <gltf
+                src="/table/grass_forest.glb"
+                :rotation-y="Math.PI * 0.5"
+                :position="getPosition(0, -2)"
+            />
+            <gltf
+                src="/table/grass_forest.glb"
+                :rotation-y="Math.PI * 0.5"
+                :position="getPosition(0, 2)"
+            />
+            <gltf
+                src="/table/building_market.glb"
+                :rotation-y="Math.PI * 0.5"
+                :position="getPosition(-2, 0)"
+            />
+            <gltf
+                src="/table/building_market.glb"
+                :rotation-y="Math.PI * 0.5"
+                :position="getPosition(-1, 1)"
+            />
+            <gltf
+                src="/table/grass_forest.glb"
+                :rotation-y="Math.PI * 0.5"
+                :position="getPosition(-1, -1)"
+            />
+        </group>
     </TroisCanvas>
 </template>
 
@@ -27,6 +72,21 @@ export default defineComponent({
     components: { OrbitControlsWrapper },
     data() {
         return { sRGBEncoding }
+    },
+    methods: {
+        getPosition(x: number, z: number) {
+            return [x * 0.86, 0, z * 0.495]
+        },
+        setupShadows(v: any) {
+            console.log(v)
+            const mesh: THREE.Mesh = v.instance
+            mesh.traverse((m: any) => {
+                if (m.isMesh) {
+                    m.castShadow = true
+                    m.receiveShadow = true
+                }
+            })
+        },
     },
 })
 </script>
