ae871e7f352068142f4ba0f9fe8121055369c653
Sander Moolin <sandermoolin@gmail.com>
2021-07-26 12:06:40 -0400

    update

--- vite-lib.config.ts
+++ vite-lib.config.ts
@@ -1,17 +1,16 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 import dts from 'vite-dts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [vue(), dts()],
+    plugins: [dts()],
     build: {
         lib: {
             entry: './src/renderer/index.ts',
-            name: 'TroisRenderer',
+            name: 'trois-renderer-proof-of-concept',
         },
         rollupOptions: {
-            external: ['lodash', 'three', 'vue'],
+            external: ['lodash', 'three'],
         }
     }
 })
