87f0d20419fc8b656df40b74735062c0251bc97a
Sander Moolin <sandermoolin@gmail.com>
2021-07-26 09:03:30 -0400

    wip on instance ocean

--- src/examples/InstanceOcean.vue
+++ src/examples/InstanceOcean.vue
@@ -1,8 +1,8 @@
 <template>
-    <TroisCanvas background="white" :cameraPosition="[0, 1.5, 3]">
+    <TroisCanvas background="white" :cameraPosition="[0, 1.4, 3]">
         <!-- <OrbitControlsWrapper :autoRotate="false" /> -->
 
-        <!-- <spotLight color="white" :intensity="1" :position="[0, 150, 0]" /> -->
+        <spotLight :intensity="1" :position="[0, 150, 0]" />
         <!-- <spotLight color="green" :intensity="0.5" :position="[0, -150, 0]" /> -->
 
         <instancedMesh :args="[null, null, count]" :scale="0.3" ref="mesh">
@@ -14,13 +14,15 @@
 
  <script lang="ts">
 import { defineComponent } from 'vue'
-import { Object3D, InstancedMesh, Vector3 } from 'three'
+import { Color, Object3D, InstancedMesh, Vector3 } from 'three'
 import OrbitControlsWrapper from './OrbitControlsWrapper.vue'
 import SimplexNoise from 'simplex-noise'
+import { keyframes } from 'popmotion'
 
 const scratch = new Object3D()
 const objects = [] as Array<any>
 const simplex = new SimplexNoise()
+const scratchColor = new Color('#ae3b4a')
 
 export default defineComponent({
     components: { OrbitControlsWrapper },
@@ -28,9 +30,23 @@ export default defineComponent({
         return {
             count: 2000,
             rotation: [0, 0, 0],
+            color: '#ffffff',
+            colors: null as any,
         }
     },
     mounted() {
+        this.colors = keyframes({
+            values: [
+                '#ae3b4a',
+                '#9e809a',
+                '#2895d6',
+                '#efdd7c',
+                '#a0e0ff',
+                '#e1eaf1',
+            ],
+        }).start((v: any) => (this.color = v))
+        this.colors.stop()
+
         const target = (this.$refs.mesh as any).$el.instance as InstancedMesh
         const columns = 20
         // const totalRows = Math.floor(this.count / columns)
@@ -55,8 +71,11 @@ export default defineComponent({
             scratch.position.set(x, y, z)
             scratch.updateMatrix()
             target.setMatrixAt(i, scratch.matrix)
+            target.setColorAt(i, scratchColor)
         }
-
+        if (target.instanceColor) {
+            target.instanceColor.needsUpdate = true
+        }
         target.instanceMatrix.needsUpdate = true
 
         this.update()
@@ -67,32 +86,39 @@ export default defineComponent({
 
             const target = (this.$refs.mesh as any).$el
                 .instance as InstancedMesh
+            if (!target) return
 
             const now = Date.now() * -0.005
 
             for (let i = 0; i < objects.length; i++) {
                 const obj = objects[i]
                 const { coords } = obj
+                const noise =
+                    simplex.noise2D(
+                        (coords[0] + now) * 0.1,
+                        (coords[1] + now) * 0.2
+                    ) *
+                        0.5 +
+                    0.5
+                const scale = noise * 0.5 + 0.01
+
                 scratch.position.set.apply(scratch.position, obj.position)
                 scratch.rotation.x = obj.rotation.x =
                     obj.rotation.x + obj.rotationSpeed.x
                 scratch.rotation.y = obj.rotation.y =
                     obj.rotation.y + obj.rotationSpeed.y
-                scratch.scale.setScalar(
-                    (simplex.noise2D(
-                        (coords[0] + now) * 0.1,
-                        (coords[1] + now) * 0.2
-                    ) *
-                        0.5 +
-                        0.5) *
-                        0.5 +
-                        0.01
-                )
+                scratch.scale.setScalar(scale)
                 scratch.updateMatrix()
                 target.setMatrixAt(i, scratch.matrix)
+                ;(this.colors as any).seek(noise)
+                scratchColor.set(this.color)
+                target.setColorAt(i, scratchColor)
             }
 
             target.instanceMatrix.needsUpdate = true
+            if (target.instanceColor) {
+                target.instanceColor.needsUpdate = true
+            }
         },
     },
 })
