ef410b85f7e3f7e7a5b1edc1a65803e2d0a844d2
Sander Moolin <sandermoolin@gmail.com>
2021-07-28 16:16:46 -0400

    wip on calendar

--- examples/calendar/Calendar.vue
+++ examples/calendar/Calendar.vue
@@ -1,10 +1,16 @@
 <template>
-    <TroisCanvas :cameraPosition="[0, 0, 20]">
+    <TroisCanvas background="beige" :cameraPosition="[0, 0, 20]">
         <OrbitControlsWrapper :autoRotate="false" />
 
         <!-- calendar -->
-        <group>
+        <group :position-x="-3.3" :position-y="2.2">
             <!-- TODO: month name -->
+            <mesh v-if="loaded" :position-x="-0.275" :position-y="1.2">
+                <textGeometry
+                    :args="[month, { font, size: 0.5, height: 0.02 }]"
+                />
+                <meshBasicMaterial color="#1D1F20" />
+            </mesh>
 
             <!-- days -->
             <!-- TODO: separate component for calendar days -->
@@ -14,33 +20,42 @@
                 :key="i"
                 :position-x="pos.x"
                 :position-y="pos.y"
-            />
+                :scale="0.5"
+            >
+                <meshBasicMaterial color="white" />
+            </mesh>
         </group>
     </TroisCanvas>
 </template>
 
 <script lang="ts">
-import { defineComponent, reactive } from 'vue'
+import { defineComponent, reactive, ref } from 'vue'
 import OrbitControlsWrapper from '../../src/examples/OrbitControlsWrapper.vue'
-import { days, getDayPositions } from './utils'
+import { days, getDayPositions, months } from './utils'
+import { FontLoader } from 'three'
 
 export default defineComponent({
     components: {
         OrbitControlsWrapper,
     },
     setup() {
-        // const now = new Date()
-        // const date = reactive({
-        //     month: now.getMonth(),
-        //     date: now.getDate(),
-        //     day: now.getDay(),
-        //     dayName: days[now.getDay()],
-        // })
-
         return {
-            // date,
-            positions: getDayPositions(),
+            positions: reactive(getDayPositions()),
+            loaded: ref(false),
+            font: reactive({}),
         }
+    },
+    data() {
+        return {
+            month: months[new Date().getMonth()],
+        }
+    },
+    mounted() {
+        const loader = new FontLoader()
+        loader.load('/calendar/gentilis_regular.typeface.json', (font) => {
+            this.loaded = true
+            this.font = font
+        })
     },
 })
 </script>
