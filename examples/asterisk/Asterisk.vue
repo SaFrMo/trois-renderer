8f4e0894f71243f10b744d554a2003b72386fd44
Sander Moolin <sandermoolin@gmail.com>
2021-08-05 09:47:02 -0400

    start on asterisk

---
+++ examples/asterisk/Asterisk.vue
@@ -0,0 +1,25 @@
+<template>
+    <TroisCanvas
+        :rendererOptions="{ alpha: true, antialias: true }"
+        :cameraPosition="[2, 2, 5]"
+    >
+        <OrbitControlsWrapper :autoRotate="false" />
+        <AsteriskComponent />
+    </TroisCanvas>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import OrbitControlsWrapper from '../../src/examples/OrbitControlsWrapper.vue'
+import AsteriskComponent from './AsteriskComponent.vue'
+
+export default defineComponent({
+    components: { AsteriskComponent, OrbitControlsWrapper },
+})
+</script>
+
+<style >
+#app > div {
+    background: radial-gradient(ellipse at center, #dae6f2 0%, #899199 100%);
+}
+</style>
---
+++ examples/asterisk/AsteriskComponent.vue
@@ -0,0 +1,46 @@
+<template>
+    <mesh ref="parent">
+        <meshBasicMaterial :wireframe="true" />
+
+        <mesh :scale="0.5" ref="child" :visible="childVisible">
+            <meshBasicMaterial color="red" :wireframe="true" />
+        </mesh>
+    </mesh>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    data() {
+        return {
+            childVisible: false,
+        }
+    },
+    mounted() {
+        console.log((this.$refs.parent as any).instance)
+
+        this.update()
+
+        setInterval(() => (this.childVisible = !this.childVisible), 800)
+    },
+    methods: {
+        update() {
+            requestAnimationFrame(this.update)
+
+            const newX = Math.sin(Date.now() * 0.001)
+
+            if (!this.$refs.parent || !this.$refs.child) return
+
+            const instance: THREE.Mesh = (this.$refs.parent as any).instance
+            instance.position.x = newX
+
+            if (!this.$refs.child) return
+
+            const child: THREE.Mesh = (this.$refs.child as any).instance
+            child.position.x = newX
+            child.position.y = Math.cos(Date.now() * 0.001)
+        },
+    },
+})
+</script>
