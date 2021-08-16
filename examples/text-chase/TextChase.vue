49d201611d89b8dff5ef06517a59839fc67977bf
Sander Moolin <sandermoolin@gmail.com>
2021-08-16 16:05:06 -0400

    starting text-chase

---
+++ examples/text-chase/TextChase.vue
@@ -0,0 +1,16 @@
+<template>
+    <TroisCanvas :cameraPosition="[0, 0, 5]">
+        <OrbitControlsWrapper :autoRotate="false" />
+        <TextChaseComponent />
+    </TroisCanvas>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import OrbitControlsWrapper from '../../src/examples/OrbitControlsWrapper.vue'
+import TextChaseComponent from './TextChaseComponent.vue'
+
+export default defineComponent({
+    components: { TextChaseComponent, OrbitControlsWrapper },
+})
+</script>
---
+++ examples/text-chase/TextChaseComponent.vue
@@ -0,0 +1,28 @@
+<template>
+    <group>
+        <mesh>
+            <torusKnotGeometry :args="[1, 0.2, 300, 20]" />
+            <meshStandardMaterial />
+        </mesh>
+
+        <hemisphereLight :key="colors[0]" :args="[colors[0], colors[1]]" />
+    </group>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import chroma from 'chroma-js'
+
+export default defineComponent({
+    setup() {
+        const colors = ref(['#60917e', '#4e64b4'])
+        const randomizeColors = () =>
+            (colors.value = [chroma.random().hex(), chroma.random().hex()])
+
+        return {
+            colors,
+            randomizeColors,
+        }
+    },
+})
+</script>
