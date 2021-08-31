2a2cc9190539e72910b378cce33cda0f64fd8a00
Sander Moolin <sandermoolin@gmail.com>
2021-08-31 12:58:20 -0400

    updated build

--- utils/lib-rollup.ts
+++ utils/lib-rollup.ts
@@ -1,21 +1,60 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+import typescript from 'rollup-plugin-typescript2'
 
-export default {
-    input: 'dist/index.js',
+const universals = {
+    input: 'src/renderer/index.ts',
     output: {
-        file: 'dist/bundle.js',
-        format: 'umd',
-        name: 'TroisRenderer',
         globals: {
             vue: 'Vue',
             three: 'THREE',
             lodash: 'lodash',
         },
     },
-    plugins: [nodeResolve()],
+    plugins: [nodeResolve(), typescript({
+        tsconfig: 'utils/tsconfig.lib.json',
+    })],
     external: [
         'lodash',
         'three',
         'vue',
     ],
 }
+
+export default [
+    // esm (module)
+    {
+        ...universals,
+        output: [
+            {
+                ...universals.output,
+                format: 'esm',
+                file: 'dist/trois.module.js'
+            },
+        ],
+    },
+    // umd
+    {
+        ...universals,
+        output: {
+            ...universals.output,
+            format: 'umd',
+            name: 'TroisRenderer',
+            file: 'dist/trois.js',
+        },
+    },
+    // min
+    {
+        ...universals,
+        plugins: [
+            ...universals.plugins,
+            terser(),
+        ],
+        output: {
+            ...universals.output,
+            format: 'umd',
+            name: 'TroisRenderer',
+            file: 'dist/trois.min.js',
+        },
+    },
+]
