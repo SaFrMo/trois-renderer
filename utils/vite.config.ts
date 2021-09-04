e6fc768ac72f0de7e29805808e7f4c3e4f66acb4
Sander Moolin <sandermoolin@gmail.com>
2021-09-04 08:12:29 -0400

    html example

--- utils/vite.config.ts
+++ utils/vite.config.ts
@@ -3,15 +3,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({
-    template: {
-      compilerOptions: {
-        isCustomElement: (tag) => {
-          return tag === 'TroisCanvas'
-        }
-      }
-    }
-  })],
+  plugins: [vue()],
   root: './examples',
   build: {
     rollupOptions: {
