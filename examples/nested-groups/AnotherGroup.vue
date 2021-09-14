d69785586ff64a9623393dabe7bec4d7784a0167
Sander Moolin <sandermoolin@gmail.com>
2021-09-14 13:07:04 -0400

    reproduction

---
+++ examples/nested-groups/AnotherGroup.vue
@@ -0,0 +1,21 @@
+<template>
+    <group :position-x="-5" :position-y="5">
+        <group v-for="i in rows" :key="i" :position-y="i * -1.5">
+            <mesh v-for="j in cols" :key="j" :position-x="j * 1.5">
+                <meshBasicMaterial color="orange" />
+            </mesh>
+        </group>
+    </group>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from 'vue'
+
+const orangeVisible = ref(true)
+onMounted(() => {
+    setInterval(() => (orangeVisible.value = !orangeVisible.value), 600)
+})
+
+const rows = 5,
+    cols = 5
+</script>
---
+++ examples/nested-groups/Component.vue
@@ -0,0 +1,15 @@
+<template>
+    <TroisCanvas :cameraPosition="[0, 0, 10]">
+        <template v-if="condition">
+            <mesh />
+            <mesh :position-x="2" />
+        </template>
+    </TroisCanvas>
+</template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const condition = ref(false)
+onMounted(() => setInterval(() => (condition.value = !condition.value), 500))
+</script>
