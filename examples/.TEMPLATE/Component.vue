ee86f4415f6c4a970b35b72e23883b122a0339ab
Sander Moolin <sandermoolin@gmail.com>
2021-09-14 12:54:13 -0400

    added example template

---
+++ examples/.TEMPLATE/Component.vue
@@ -0,0 +1,10 @@
+<template>
+    <TroisCanvas :cameraPosition="[0, 0, 10]">
+        <OrbitControlsWrapper />
+        <mesh />
+    </TroisCanvas>
+</template>
+
+<script lang="ts" setup>
+import OrbitControlsWrapper from '../../src/examples/OrbitControlsWrapper.vue'
+</script>
